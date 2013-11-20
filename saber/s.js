a749650c27d7ad956ee01b1eee895ebf8d7f209f
unknown <v-Qun@.(none)>
2013-11-20 15:48:58 +0800

    test seajs.require

--- saber/s.js
+++ saber/s.js
@@ -1,6 +1,5 @@
 define("s", function(require, exports, module) {
 	var router = require("router");
 	var routerInfo = router.parse(location.href);
-	seajs.require(routerInfo.pathname.replace("#test/", ""));
-
+	seajs.require(routerInfo.hash.replace("#test/", ""));
 });
