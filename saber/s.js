e1887057be1f4ebd5280a5184fe5150971e48f48
unknown <v-Qun@.(none)>
2013-11-20 15:17:22 +0800

    test seajs.require

--- saber/s.js
+++ saber/s.js
@@ -1,8 +1,6 @@
 define("s", function(require, exports, module) {
 	var router = require("router");
 	var routerInfo = router.parse(location.href);
-	// console.log(routerInfo);
-	debugger;
 	seajs.require(routerInfo.pathname.replace("#test/", ""));
 
 });
