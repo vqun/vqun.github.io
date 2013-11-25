9390738f835df5f9b5d42cf7bf2f37329c5ad3d7
unknown <v-Qun@.(none)>
2013-11-25 16:50:01 +0800

    use jquery for testin

--- saber/fs.js
+++ saber/fs.js
@@ -1,12 +1,18 @@
 define("fs", function(require, exports, module) {
 	// fs.read
-	var utils = require("utils");
+	// var utils = require("utils");
+	var J = require("jQuery");
+	var $ = J.jQuery;
 	exports.read = function(file, callback) {
-		var args = {
-			"url": file,
-			"onComplete": callback,
-			"method": "post" // file read force post
-		};
-		utils.ajax(args);
+		// var args = {
+		// 	"url": file,
+		// 	"onComplete": callback,
+		// 	"method": "post" // file read force post
+		// };
+		// utils.ajax(args);
+		$.ajax(file, {
+			"dataType": "json",
+			"success": callback
+		});
 	};
 });
